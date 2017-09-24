09d546ab71185030ca07c38bdbb83f07003aa12c
Дмитрий <m-dimon93@yandex.ru>
2017-09-24 17:13:01 +0400

    Механизм работает, теперь можно добавлять функционал и красоту наводить

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function createHero() {
+    var heroName = $("#book-name").val();
+
+    var request = {
+        type: "POST",
+        url: "/booksUniqueCreate",
+        headers: {
+            'Accept': 'application/json;charset=UTF-8',
+            'Content-Type': 'application/json;charset=UTF-8'
+        },
+        data: JSON.stringify({
+            name: bookName
+        })
+    };
+    $.ajax(request)
+        .done(function () {
+            alert("Book created");
+        })
+        .fail(function (response) {
+            alert("Error: " + response.responseJSON.message);
+        });
+}
